164652750f3fccce589bf1f4b7260a4e77ae209c
Austin Meadows <austin-meadows@users.noreply.github.com>
2021-08-06 22:23:43 -0400

    gen both

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
+const path = require("path");
+const zlib = require("zlib");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 
 const paths = {
@@ -104,7 +105,24 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new CompressionPlugin({
+      filename: "[path][base].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 0,
+      minRatio: 0.8,
+    }),
+    new CompressionPlugin({
+      filename: "[path][base].br[query]",
       algorithm: "brotliCompress",
+      test: /\.(js|css|html|svg)$/,
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+      threshold: 0,
+      minRatio: 0.8,
+      deleteOriginalAssets: false,
     }),
     new HtmlWebpackPlugin({
       inject: true,
