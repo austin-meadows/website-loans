6d99e1a20936cf55e614d454300b3c1a6620b605
Austin Meadows <austin@slowp.ke>
2022-01-21 20:31:30 -0500

    sort imports

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,10 @@
-// Import rollup plugins
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
 import { copy } from "@web/rollup-plugin-copy";
-import { terser } from "rollup-plugin-terser";
 import html from "@web/rollup-plugin-html";
 import minifyHTML from "rollup-plugin-minify-html-literals";
-import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 import summary from "rollup-plugin-summary";
-import typescript from "@rollup/plugin-typescript";
 
 const isProd = process.env.NODE_ENV === "production";
 
